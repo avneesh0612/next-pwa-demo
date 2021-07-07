28ebcc2155b2d19fda61ff245544a47096cb1eb1
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-07-07 19:10:27 +0530

    done with the tutorial

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
